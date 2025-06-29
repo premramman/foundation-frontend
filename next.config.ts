597759388656a074837820fa13dfd6e71a781524
premramman <premramman@gmail.com>
2025-06-29 18:57:26 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
-import {NextConfig} from 'next';
+import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['picsum.photos'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '/**', // Allow all paths from this domain
+      },
+    ],
   },
 };
- 
+
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
